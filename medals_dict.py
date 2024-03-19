1d3cdf60994afdc4e9e65be25022143991d8b6ad
enesodunkesen <enesodunkesen@hotmail.com>
2024-03-19 23:21:56 +0300

    writing to a file using `DictWriter` , excluding fieldnames keys using `extrasaction` kwarg and writing with sorted order

--- medals_dict.py
+++ medals_dict.py
@@ -1,5 +1,9 @@
 import csv
 
+# When writing CSV data with a DictWriter, we have to tell it which columns to include, and which order we want to write
+# If you want to skip a column, and not have it written to the file, then you have to pass 'ignore' as the argument
+# for the extrasaction parameter. If you don't do that, you'll get a ValueError
+# when the dictionaries contain keys that aren't listed.
 medals_table = [
     {'country': 'United States', 'gold': 39, 'silver': 41, 'bronze': 33, 'rank': 1},
     {'country': 'China', 'gold': 38, 'silver': 32, 'bronze': 18, 'rank': 2},
@@ -95,3 +99,36 @@ medals_table = [
     {'country': 'Moldova', 'gold': 0, 'silver': 0, 'bronze': 1, 'rank': 86},
     {'country': 'Syria', 'gold': 0, 'silver': 0, 'bronze': 1, 'rank': 86},
 ]
+
+# the easiest way(for using DictWriter) is to create a list, containing the keys that we want to include in the CSV file:
+# We'll pass that list as the fieldnames argument, when we create the DictWriter.
+
+fieldnames = ['country', 'gold', 'silver', 'bronze', 'ignore']
+file_name = "country_medals.csv"
+
+with open(file_name, "w", encoding="utf-8", newline="") as file:
+    writer = csv.DictWriter(file, fieldnames=fieldnames, extrasaction='ignore')
+    # can exclude some keys, if you don't want those values to appear in the CSV file.
+    # to do that we should change the key in fieldnames with 'ignore' and use extrasaction kwarg
+    writer.writeheader()
+    # If you don't call the writeheader method,you won't get the headings in the CSV file.
+
+    # for row in medals_table:
+    #     writer.writerow(row)
+
+    # writer.writerows(sorted(return_values(medals_table)))
+
+    # There are two ways you can use a DictWriter to write the data.
+    # You can either iterate over your sequence of dictionaries, and write the data row by row,(line114)
+    # or you can send the entire sequence to the writer.(line 117)
+
+    def return_values(dictionary: dict):
+        return dictionary['country']
+
+
+    # When you sort things, the sorting function has to compare them, to decide what order they go in.
+    # The less than operator isn't defined for dicts how would you decide if one dictionary is less than or greater ?
+    # Because they can't be compared, they can't be sorted.What we have to do,
+    # is create a small function that returns the value that we want to sort on.
+    # The sorted function will then call that function, to get the values that can be compared.
+    writer.writerows(sorted(medals_table, key=return_values))
